6e9e4a1960acba6922b60756f21aec5744d69ab2
SolStonez <stonez.solana@gmail.com>
2024-05-29 09:21:17 -0700

    add names to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
 
 const Home: React.FC = () => {
+  
+  const facilityNames: { [key: number]: string } = {
+    1: 'Gridley Post Acute',
+    2: 'Example Facility 2',
+    3: 'Example Facility 3',
+    4: 'Example Facility 4',
+    5: 'Example Facility 5',
+    6: 'Example Facility 6',
+    7: 'Example Facility 7',
+    8: 'Example Facility 8',
+  };
+  
+  
   return (
     <div>
       <h1>Maintenance Reports</h1>
       <ul>
         {[1, 2, 3, 4, 5, 6, 7, 8].map((facility) => (
           <li key={facility}>
-            <Link href={`/facility/${facility}`}>{facility}</Link>
+            <Link href={`/facility/${facility}`}>{facilityNames[facility]}</Link>
           </li>
         ))}
       </ul>
